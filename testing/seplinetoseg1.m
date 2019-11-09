5b649c631bb21a2f7883efed85e13c2ba9b96219
Katesopon <karaakate7214@gmail.com>
2019-11-09 21:08:05 +0700

    dont seperate each line

--- testing/seplinetoseg1.m
+++ testing/seplinetoseg1.m
@@ -1,3 +1,6 @@
+% seplinetoseg1.m
+% this is for save each line into cell rows
+% by seperating each variables(X, Y and Z)
 SegX = cell(1);
 SegY = cell(l);
 SegZ = cell(l);
@@ -12,8 +15,8 @@ for i = 0:l
             SegY{1, j} = Yci{j};
             SegZ{1, j} = Zc{j};
         end
-    elseif i == l(end)
-        for j = sepline(i):
+    %elseif i == l(end)
+    %    for j = sepline(i)
     else
         for j = sepline(i):sepline(i+1)-1
             SegX{i+1, j-(sepline(i)-1)} = Xc{j};
